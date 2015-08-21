27e95d28b42833575d4f8281be717f3d76d33597
LunarDevelopment <development.lunar@gmail.com>
2015-08-21 16:59:26 +0100

    windows stuff

--- inventory-app/app/scripts/controllers/about.js
+++ inventory-app/app/scripts/controllers/about.js
@@ -7,20 +7,27 @@
  * # AboutCtrl
  * Controller of the inventoryApp
  * @requires Twitter Factory
+ * @requires $window to access global Materialize library's Toasts
  * @property {Array} tweets
  * @property {twitter} new instance of Twitter factory
  * @property {Object} style object of progress bar
  */
 angular.module('inventoryApp')
-  .controller('AboutCtrl', function (Twitter) {
+  .controller('AboutCtrl', function (Twitter, $window) {
     var vm = this;
     vm.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    vm.exampleToast = {
+      duration: 1000,
+      message: 'You reached out!'
+    };
     vm.twitter = new Twitter();
     vm.tweets = null;
+    vm.exampleTweet = '';
+    vm.searchTerm = '';
     vm.progress = {
       width: '0%'
     };
@@ -36,8 +43,13 @@ angular.module('inventoryApp')
         vm.progress.width = '25%';
       }
     };
+    /**
+     * @description
+     * Emulates a call to Twitter's API to get tweets related to search term
+     * @param {String} searchTerm is pulled from model to run search.
+     */
     vm.exampleSearch = function () {
-      if (vm.searchTerm.length !== 0) {
+      if (vm.searchTerm.length) {
         vm.progress.width = '50%';
       }
     };
@@ -54,7 +66,10 @@ angular.module('inventoryApp')
      * @description Emulates sending a tweet to potential client
      *
      */
-    vm.sendExample = function () {
+    vm.sendExample = function (tweet) {
+      tweet.classes = 'purple lighten-5';
+      tweet.disabled = true;
+      $window.Materialize.toast('You reached out!', 3000);
       vm.progress.width = '100%';
     };
   });
